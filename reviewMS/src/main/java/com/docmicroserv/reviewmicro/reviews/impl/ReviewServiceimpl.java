902c8490acaa9848fd72bf0105448cfc62ad5fb8
GanaaMT <tab.erd.ganerdene@gmail.com>
2026-03-03 00:01:26 +0800

    reviews micro service completed

---
+++ reviewMS/src/main/java/com/docmicroserv/reviewmicro/reviews/impl/ReviewServiceimpl.java
@@ -0,0 +1,71 @@
+package com.docmicroserv.reviewmicro.reviews.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.docmicroserv.reviewmicro.reviews.Review;
+import com.docmicroserv.reviewmicro.reviews.ReviewRepository;
+import com.docmicroserv.reviewmicro.reviews.ReviewService;
+
+@Service
+public class ReviewServiceimpl implements ReviewService{
+    private final ReviewRepository reviewRepository;
+
+    
+
+   public ReviewServiceimpl(ReviewRepository reviewRepository) {
+    this.reviewRepository = reviewRepository;
+    }
+    
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        
+        if (companyId != null && review != null) {
+            review.setCompanyId(companyId);
+            reviewRepository.save(review);
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    @Override
+    public Review getReview(Long reviewId) {
+        return reviewRepository.findById(reviewId).orElse(null);
+    }
+
+    @Override
+    public boolean updateReview( Long reviewId, Review updatedReview) {
+    Review review = reviewRepository.findById(reviewId).orElse(null);
+    
+    if (review != null) {
+        review.setTitle(updatedReview.getTitle());
+        review.setDescription(updatedReview.getDescription());
+        review.setRating(updatedReview.getRating());
+        review.setCompanyId(updatedReview.getCompanyId());
+        reviewRepository.save(review);
+        return true;
+    }
+    return false;
+}
+
+    @Override
+    public boolean deleteReview( Long reviewId) {
+
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        
+        if (review != null) {
+            reviewRepository.delete(review);
+            return true;
+            }
+        return false;
+    }
+    
+}
